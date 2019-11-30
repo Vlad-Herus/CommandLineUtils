6c8ff4660b824d37f26a81272c86227cf8214f8a
Vlad <VladimirGerusUA@gmail.com>
2019-11-30 16:55:59 +1100

    added folder structure, enhancements for brListFiltered.bat

--- brListFiltered.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-if %2.==. (brlist.bat %1) else (brlist.bat %1|findstr -i %2)
---
+++ git/brListFiltered.bat
@@ -0,0 +1,25 @@
+@echo off
+
+rem can be called with two parameters %1 is number of recent branches to be fetched from the server; %2 is filter to be aplied on top of fetched branhces
+rem can be called with one parameter : 
+rem if %1 is numeric it will be used as a number of recent brancehs to be fetched from the server
+rem if %1 is nut numeric it will be used a a filter for fetched branhces with number of branches = 65536
+
+if %2.==. (
+
+  SET "var="&for /f "delims=0123456789" %%i in ("%1") do set var=%%i
+  if defined var (
+    rem character is numeric. If all are numeric we're passing %1 as count parameter to brList
+    brlist.bar 65536|findstr -i %1
+    GOTO:EOF 
+  ) else (
+    rem character is numeric. If all are numeric we're passing %1 as count parameter to brList
+  )
+
+  brlist.bat %1
+
+) else (
+
+brlist.bat %1|findstr -i %2
+
+)
